8634c63eb867fe2b4f97f3d77776900139e89f2a
madajaju <darren@pulsipher.org>
2018-10-05 09:44:37 -0700

    Update Jenkins file to handle new doc build.

--- deploy/build.js
+++ deploy/build.js
@@ -1,47 +1,56 @@
 const path = require('path');
 const fs = require('fs');
 const childProcess = require('child_process');
-const cmd = process.argv[3]; // node, build, cmd
+const cmd = process.argv[2]; // node, build, cmd
 const root = process.cwd() + '/deploy/uservices';
-npmBuildRecursive(root);
 
+console.log('CMD:', cmd);
 // it will be `npm run-script build` inside root in the end.
 console.log('===================================================================');
-console.log(`Performing "npm ${cmd}" inside root folder: ` + root);
+console.log('Performing npm ' + cmd + ' inside root folder: ' + root );
 console.log('===================================================================');
 
-function npmBuildRecursive(folder) {
+npmBuildRecursive(root);
+
+function npmBuildRecursive(folder)
+{
   console.log('NPM Build Recursive: ' + folder);
 
-  for (let subfolder of subfolders(folder)) {
+  for (let subfolder of subfolders(folder))
+  {
     npmBuildRecursive(subfolder);
   }
 
-  const hasPackageJson = fs.existsSync(path.join(folder, 'package.json'));
+  const hasPackageJSON = fs.existsSync(path.join(folder, 'package.json'));
 
-  console.log('has package: ' + hasPackageJson);
-  if (!hasPackageJson && path.basename(folder) !== 'code') {
+  console.log('has package: ' + hasPackageJSON);
+  if (!hasPackageJSON && path.basename(folder) !== 'code')
+  {
     return;
   }
 
-  if (folder !== root && hasPackageJson) {
+  if (folder !== root && hasPackageJSON)
+  {
     console.log('===================================================================');
-    console.log(`Performing "npm ${cmd}" inside ${folder === root ? 'root folder' : './' + path.relative(root, folder)}`);
+    console.log(`Performing "npm run ${cmd}" inside ${folder === root ? 'root folder' : './' + path.relative(root, folder)}`);
     console.log('===================================================================');
 
     npmBuild(folder);
   }
 
-  for (let subfolder of subfolders(folder)) {
+  for (let subfolder of subfolders(folder))
+  {
     npmBuildRecursive(subfolder);
   }
 }
 
-function npmBuild(where) {
-  childProcess.execSync('npm run-script ${cmd}', {cwd: where, env: process.env, stdio: 'inherit'});
+function npmBuild(where)
+{
+  childProcess.execSync('npm run-script ' + cmd, { cwd: where, env: process.env, stdio: 'inherit' });
 }
 
-function subfolders(folder) {
+function subfolders(folder)
+{
   return fs.readdirSync(folder)
     .filter(subfolder => fs.statSync(path.join(folder, subfolder)).isDirectory())
     .filter(subfolder => subfolder !== 'node_modules' && subfolder[0] !== '.')
